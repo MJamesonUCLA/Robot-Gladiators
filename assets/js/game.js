37163ba61b9764500e272d986fb08338583212b3
MJamesonUCLA <85082925+MJamesonUCLA@users.noreply.github.com>
2021-09-30 14:30:50 -0700

    branches

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+// this creates a function named "fight"
+
+var playerName = window.prompt("what is your robot's name?"); {
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+//this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log ("Our robot's name is " + playerName);
+    window.alert("The fight has begun!");
+var name = "your name";
+console.log(name);
+}
+
+// fight();
+
